b87e453c2c90920ea05de7404a1d0eab3ca42737
Akhil Gandhi <akga5282@colorado.edu>
2022-12-10 17:59:34 -0700

    Updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,73 @@
-# need ubuntu-alpine.
-FROM ubuntu:focal
+FROM ubuntu:18.04
 
-RUN mkdir /app
+#Update repo and upgrade 
+RUN apt-get update && apt-get upgrade -y
 
-WORKDIR /app
+#Install sudo
+RUN apt install sudo tasksel -y
 
-COPY install.sh install.sh
-
+#Set Timezone var
 ENV TZ=America/Denver
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-RUN chmod a+x install.sh
-RUN ./install.sh
+#Install python
+RUN sudo apt-get install python3.8 -y
 
+#Install utilities
+RUN apt-get update
+RUN apt install python3-pip -y  
+RUN apt install curl -y 
+RUN apt install unzip -y 
+RUN apt install wget -y
+
+#Install aws cli prereq
+RUN sudo apt install glibc-source -y && \
+	sudo apt-get install groff -y && \
+	sudo apt-get install less -y
+	
+#Install AWS cli 
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+RUN unzip awscliv2.zip
+RUN sudo ./aws/install
+
+#Configure AWS cli
 COPY config ~/.aws/
 COPY credentials ~/.aws/
+
+#Install git
+RUN apt-get update -y
+RUN apt-get update && apt-get install git-all -y
+
+#Install Miniconda 
+ENV PATH="/root/miniconda3/bin:${PATH}"
+ARG PATH="/root/miniconda3/bin:${PATH}"
+RUN wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+    && mkdir /root/.conda \
+    && bash Miniconda3-latest-Linux-x86_64.sh -b \
+    && rm -f Miniconda3-latest-Linux-x86_64.sh \
+    && echo "Running $(conda --version)" && \
+    conda init bash && \
+    . /root/.bashrc && \
+    conda update conda
 
 COPY app.py app.py
 
 COPY A2.jpg A2.jpg
+
 COPY requirements.txt requirements.txt
 
 RUN pip install -r requirements.txt
 
-CMD ["gunicorn", "-w 4", "app:app"]
+RUN git clone https://github.com/AkGandhi99/stable-diffusion.git
+
+WORKDIR /stable-diffusion
+
+RUN conda env update -n base --file environment.yaml
+
+RUN curl https://f004.backblazeb2.com/file/aai-blog-files/sd-v1-4.ckpt > sd-v1-4.ckpt
+
+WORKDIR /
+
+CMD ["python3", "app.py"]
+
+ENTRYPOINT [ "/bin/bash", "-l", "-c" ]
--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-import requests
+import time
 import boto3
 from botocore.exceptions import ClientError
 import logging
-from flask import Flask, request, Response, send_file
-
+from flask import Flask, request, send_file
+import re
+import os
+from glob import glob
+from io import BytesIO
+from zipfile import ZipFile
 
 
 app = Flask(__name__)
@@ -33,13 +37,14 @@ def image_generated():
 @app.route('/store', methods=['GET'])
 def store_request(bucket_name=None, region=None):
     try:
-        file_name = 'A2.jpg'
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        file_name = 'A2.jpg' #add time stamp later
         object_name = file_name
         args = request.args.to_dict()
         file_id = args['user_id']
-        bucket_name = file_id
+        bucket_name = re.sub(r'@.*', '', file_id)
         region = 'us-west-1'
-        if if_bucket_exsist(file_id):
+        if if_bucket_exsist(bucket_name):
             if region is None:
                 s3_client = boto3.client('s3')
                 s3_client.create_bucket(Bucket=bucket_name)
@@ -56,6 +61,37 @@ def store_request(bucket_name=None, region=None):
         return {'working':'failed'}
     return {'working':'ok'}
 
+@app.route('/getRecent', methods=['GET'])
+def getRecent():
+    try:
+        args = request.args.to_dict()
+        buck_n = args['user_id']
+        bucket_name = re.sub(r'@.*', '', buck_n)
+        s3 = boto3.resource('s3')
+        my_bucket = s3.Bucket(bucket_name)
+        
+        os.chdir('send_images/')
+        # download file into current directory
+        for s3_object in my_bucket.objects.all():
+            # Need to split s3_object.key into path and file name, else it will give error file not found.
+            path, filename = os.path.split(s3_object.key)
+            my_bucket.download_file(s3_object.key, filename)
+
+        stream = BytesIO()
+        with ZipFile(stream, 'w') as zf:
+            for file in glob(os.path.join('*.png')):
+                zf.write(file, os.path.basename(file))
+        stream.seek(0)
+
+        return send_file(
+            stream,
+            as_attachment=True,
+            download_name='archive.zip'
+        )
+        
+    except ClientError as e:
+        logging.error(e)
+        return {'working':'failed'}
 
 if __name__ == '__main__':
 	app.run(host = '0.0.0.0')
